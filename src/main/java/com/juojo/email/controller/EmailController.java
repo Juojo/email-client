38b308e22e1d333da0815f05ace9831cc9c8e7b7
Juojo <minarro.juanmartin@gmail.com>
2024-09-13 20:01:24 -0300

    Add sendEmail POST request to EmailController

--- src/main/java/com/juojo/email/controller/EmailController.java
+++ src/main/java/com/juojo/email/controller/EmailController.java
@@ -1,18 +1,35 @@
 package com.juojo.email.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.juojo.email.model.EmailModel;
+import com.juojo.email.service.EmailService;
+
 @RestController
+@RequestMapping("/api")
 public class EmailController {
+	
+	private EmailService emailService = new EmailService();
 
 	public EmailController() {
 		
 	}
 	
-	@GetMapping("/")
+	@GetMapping("/hello")
 	public String hello() {
 		return "Hello world!";
+	}
+	
+	@PostMapping("/sendEmail")
+	public ResponseEntity<HttpStatus> sendEmail(@RequestBody EmailModel email) {
+		HttpStatus httpStatus = emailService.sendEmail(email);
+		return new ResponseEntity<>(httpStatus);
 	}
 	
 }
